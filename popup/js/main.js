a68b9f122280d0d72e23145b7942eb3697fd934b
Vertushkins <152332518+Vertushkins@users.noreply.github.com>
2024-12-23 12:58:45 +0300

    Add files via upload

--- popup/js/main.js
+++ popup/js/main.js
@@ -1,14 +1,32 @@
-document.getElementById("pop-up").addEventListener("click", () => {
+function container_toggle() {
 	const container = document.getElementById("container");
 
 	if (container.classList.contains("hidden")){
 		container.classList.remove("hidden");
 		container.classList.add("visible");
 	}
-})
+	else {
+		container.classList.remove("visible");
+		container.classList.add("hidden");
+	}
+}
 
-document.getElementById("close").addEventListener("click", () => {
-	const container = document.getElementById("container");
-	container.classList.remove("visible");
-	container.classList.add("hidden");
-})
+const form = document.getElementById('form-container');
+const inputs = form.querySelectorAll('input, textarea');
+
+form.addEventListener('submit', () => {
+    let isValid = true;
+
+    inputs.forEach(input => {
+        if (!input.checkValidity()) {
+            isValid = false;
+        }
+    });
+
+    if (isValid) {
+        container_toggle();
+    }
+});
+
+document.getElementById("pop-up").addEventListener("click", container_toggle);
+document.getElementById("close").addEventListener("click", container_toggle);
